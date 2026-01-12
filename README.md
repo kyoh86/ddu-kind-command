9bcc5fb4f2838953b558b4726dc4882eff2f6ad6
kyoh86 <me@kyoh86.dev>
2026-01-12 12:49:21 +0900

    feat: init ddu-kind-command

---
+++ README.md
@@ -0,0 +1,5 @@
+# ddu-kind-command
+
+Ex-Command kind for ddu.vim
+
+This kind is used by ddu-source-command.
---
+++ denops/@ddu-kinds/command/main.ts
@@ -0,0 +1,27 @@
+import {
+  type ActionArguments,
+  ActionFlags,
+  type Actions,
+} from "@shougo/ddu-vim/types";
+import { BaseKind } from "@shougo/ddu-vim/kind";
+import * as fn from "@denops/std/function";
+
+type ActionData = {
+  command: string;
+};
+
+type Params = Record<never, never>;
+
+export class Kind extends BaseKind<Params> {
+  params(): Params {
+    return {};
+  }
+
+  actions: Actions<Params> = {
+    edit: async ({ denops, items }: ActionArguments<Params>) => {
+      const action = items[0]?.action as ActionData;
+      await fn.feedkeys(denops, `:${action.command}`, "n");
+      return Promise.resolve(ActionFlags.None);
+    },
+  };
+}
